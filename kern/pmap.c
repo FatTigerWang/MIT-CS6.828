a44871812ee114a7b4fd9513393ac12a9289b442
FatTiger <owner@wangshenjie.com>
2021-03-12 23:48:17 +0800

    fix bug

--- kern/pmap.c
+++ kern/pmap.c
@@ -296,18 +296,18 @@ struct PageInfo *
 page_alloc(int alloc_flags)
 {
 	if(alloc_flags & ALLOC_ZERO){
-		if(page_free_list.pp_link == NULL){
+		if(page_free_list->pp_link == NULL){
 			//内存不足
 			return NULL;
 		}
 		//分配页面
-		PageInfo *p = page_free_list;
+		struct PageInfo *p = page_free_list;
 		//分配出去的页面link设置NULL
 		p->pp_link = NULL;
 		//空闲页面指向下一个页面
 		page_free_list = page_free_list->pp_link;
 		//初始化分配出去的页内存
-		memset(page2kva(p), 0, sizeof(struct PageInfo));
+		memset((int)page2kva(p), 0, sizeof(struct PageInfo));
 		return p;
 	}
 	
